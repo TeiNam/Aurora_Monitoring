b7c804ab67c51b2bb31cfaf8489016ca6d0db0f3
TeiNam <teioracle@gmail.com>
2024-03-28 17:10:14 +0900

    rds_instance.json 파일 api에서 제거 DB 내재화 수집기

--- collector/mysql_command_status.py
+++ collector/mysql_command_status.py
@@ -1,11 +1,8 @@
 import asyncio
-import asyncmy
 import pytz
 from datetime import datetime
-from modules.crypto_utils import decrypt_password
+from modules.load_instance import load_instances_from_mongodb, handle_instance
 from modules.mongodb_connector import MongoDBConnector
-from modules.json_loader import load_json
-from modules.time_utils import get_kst_time
 from config import MONGODB_STATUS_COLLECTION_NAME
 
 
@@ -19,7 +16,7 @@ async def query_mysql_status(connection, query_string, single_row=False):
             else:
                 return {row[0]: row[1] for row in await cur.fetchall()}
     except Exception as e1:
-        print(f"{get_kst_time()} - Failed to query MySQL status: {e1}")
+        print(f"Failed to query MySQL status: {e1}")
         return None
 
 
@@ -33,30 +30,23 @@ def process_global_status(data, uptime):
     ]
     processed_data = {}
     total_sum = 0
-
     for key, value in data.items():
         if key in desired_commands and value != '0':
             total_sum += int(value)
-
     for key, value in data.items():
         if key in desired_commands and value != '0':
             new_key = key[4:]
             value = int(value)
-
             avg_for_hours = round(value / max(uptime / 3600, 1), 2)
             avg_for_seconds = round(value / max(uptime, 1), 2)
             percentage = round((value / total_sum) * 100, 2) if total_sum > 0 else 0
-
             processed_data[new_key] = {
                 'total': value,
                 'avgForHours': avg_for_hours,
                 'avgForSeconds': avg_for_seconds,
                 'percentage': percentage
             }
-
-    processed_data_sorted = dict(sorted(processed_data.items(), key=lambda item: item[1]['total'], reverse=True))
-
-    return processed_data_sorted
+    return dict(sorted(processed_data.items(), key=lambda item: item[1]['total'], reverse=True))
 
 
 async def save_mysql_command_status_to_mongodb(collection, instance_name, command_status):
@@ -71,44 +61,28 @@ async def save_mysql_command_status_to_mongodb(collection, instance_name, comman
 async def query_instance_and_save_to_db(connection, instance, collection):
     uptime = await query_mysql_status(connection, "SHOW GLOBAL STATUS LIKE 'Uptime';", True)
     if uptime is None:
-        print(f"{get_kst_time()} - Could not retrieve uptime for {instance['instance_name']}")
+        print(f"Could not retrieve uptime for {instance['instance_name']}")
         return
-
     raw_status = await query_mysql_status(connection, "SHOW GLOBAL STATUS LIKE 'Com_%';")
     if raw_status is None:
-        print(f"{get_kst_time()} - Could not retrieve global status for {instance['instance_name']}")
+        print(f"Could not retrieve global status for {instance['instance_name']}")
         return
-
     processed_status = process_global_status(raw_status, uptime)
     await save_mysql_command_status_to_mongodb(collection, instance["instance_name"], processed_status)
 
 
-async def handle_instance(instance, collection):
-    try:
-        decrypted_password = decrypt_password(instance["password"])
-        connection = await asyncmy.connect(
-            host=instance["host"], port=instance["port"],
-            user=instance["user"], password=decrypted_password, db=instance["db"]
-        )
-        await query_instance_and_save_to_db(connection, instance, collection)
-        await connection.ensure_closed()
-    except Exception as e2:
-        print(f"{get_kst_time()} - Failed to handle instance: {e2}")
+async def custom_handle_instance(instance, collection):
+    # `handle_instance` 함수에 대한 wrapper로, `query_instance_and_save_to_db` 함수를 인자로 전달합니다.
+    await handle_instance(instance, collection, query_instance_and_save_to_db)
 
 
 async def run_mysql_command_status():
     await MongoDBConnector.initialize()
-
     mongodb = await MongoDBConnector.get_database()
-    collection = mongodb[MONGODB_STATUS_COLLECTION_NAME]
-
-    instances = load_json('rds_instances.json')
-    tasks = [handle_instance(instance, collection) for instance in instances]
+    status_collection = mongodb[MONGODB_STATUS_COLLECTION_NAME]
+    instances = await load_instances_from_mongodb()
+    tasks = [custom_handle_instance(instance, status_collection) for instance in instances]
     await asyncio.gather(*tasks)
 
-
 if __name__ == '__main__':
-    try:
-        asyncio.run(run_mysql_command_status())
-    except Exception as ex:
-        print(f"{get_kst_time()} - An error occurred: {ex}")
+    asyncio.run(run_mysql_command_status())
---
+++ modules/load_instance.py
@@ -0,0 +1,28 @@
+# modules/load_instance.py
+
+import asyncmy
+from modules.crypto_utils import decrypt_password
+from modules.mongodb_connector import MongoDBConnector
+from config import MONGODB_RDS_INSTANCE_LIST_COLLATION_NAME
+
+
+async def load_instances_from_mongodb():
+    """MongoDB에서 RDS 인스턴스 정보를 불러옵니다."""
+    mongodb = await MongoDBConnector.get_database()
+    collection = mongodb[MONGODB_RDS_INSTANCE_LIST_COLLATION_NAME]
+    instances = await collection.find().to_list(length=None)
+    return instances
+
+
+async def handle_instance(instance, collection, query_instance_and_save_to_db):
+    """각 인스턴스에 대한 처리를 수행합니다."""
+    try:
+        decrypted_password = decrypt_password(instance["password"])
+        connection = await asyncmy.connect(
+            host=instance["host"], port=instance.get("port", 3306),
+            user=instance["user"], password=decrypted_password, db=instance.get("db", "")
+        )
+        await query_instance_and_save_to_db(connection, instance, collection)
+        await connection.ensure_closed()
+    except Exception as e:
+        print(f"Failed to handle instance: {e}")
