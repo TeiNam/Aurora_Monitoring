ec6ed073c189d1f587c07d6b83a708ea1e8c02e9
TeiNam <teinam@rastalion.dev>
2024-05-21 09:47:27 +0900

    슬랙 노티, 허용 유저 목록 제거

--- modules/slack_noti.py
+++ modules/slack_noti.py
@@ -18,12 +18,16 @@ def get_slack_user_id(email):
     return None
 
 
-def send_slack_notification(user_email, title, message):
+def send_slack_notification(user_email, title, instance_info, db_info, pid_info, execution_time):
     user_id = get_slack_user_id(user_email)
     if user_id is None:
         raise ValueError("User not found in Slack")
 
-    formatted_message = f'*{title}*\n<@{user_id}> {message}'
+    formatted_message = (f'*{title}*\n<@{user_id}> 계정으로 실행한 SQL쿼리(PID: {pid_info})가\n '
+                         f'*{instance_info}*, *{db_info}* DB에서 *{execution_time}* 초 동안 실행 되었습니다.\n'
+                         ' 쿼리 검수 및 실행 시 주의가 필요합니다. \n'
+                         f'http://{os.getenv("HOST")}:8000/sql-plan?pid={pid_info}')
+
     webhook_url = os.getenv("SLACK_WEBHOOK_URL")
     if not webhook_url:
         raise ValueError("SLACK_WEBHOOK_URL is not set in the environment variables.")
@@ -32,4 +36,5 @@ def send_slack_notification(user_email, title, message):
     response = requests.post(webhook_url, json=payload)
 
     if response.status_code != 200:
-        raise ValueError(f"Request to slack returned an error {response.status_code}, the response is:\n{response.text}")
+        raise ValueError(
+            f"Request to slack returned an error {response.status_code}, the response is:\n{response.text}")
